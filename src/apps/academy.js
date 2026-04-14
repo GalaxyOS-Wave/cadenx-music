2873d58cce5cd7c374bcba85ee242ad311bcce16
Cadenx Music. <dishantiscool236@gmail.com>
2026-04-14 10:27:23 +0530

    Update academy.js

--- src/apps/academy.js
+++ src/apps/academy.js
@@ -10,7 +10,7 @@ export const ACADEMY_DATA = [
         duration: '00 Hours',
         price: '₹0',
         cover: 'graphics/sursadhana.png',
-        thumbnail: 'graphics/sursadhana.png',
+        thumbnail: 'sursadhana.png',
         purchaseUrl: 'https://forms.google.com/purchase-batch-1',
         videos: [
             { 
